10f1271a0e087c1d4f2636f0bbda3d9ee31203ad
enhjinnn <enhjinn08@gmail.com>
2023-07-07 20:30:36 +0800

    Second

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
+import { getElementError } from '@testing-library/react';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById ('root'))
 
-reportWebVitals(sendToVercelAnalytics);
+root.render(<App/>)
