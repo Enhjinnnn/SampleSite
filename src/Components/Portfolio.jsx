f1f699d15dd783acffb64830d9dd0c14de5c9db5
enhjinnn <enhjinn08@gmail.com>
2023-07-20 14:34:30 +0800

    add scroll wrapper

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,21 +1,47 @@
 import React from 'react';
 import Projectsbox from './Projectsbox';
-import fimage1 from '../img/1.svg';
-import fimage2 from '../img/2.svg';
-import fimage3 from '../img/3.svg';
-import fimage4 from '../img/4.svg';
+import fimage1 from '../img/characters.png';
+import fimage2 from '../img/environments.png';
+import fimage3 from '../img/visualEffects.png';
+import fimage4 from '../img/models.png';
+import { useNavigate} from 'react-router-dom';
 
 
 function Portfolio() {
+  const navigate = useNavigate();
+
+  const handleButtonClick = (path) => {
+    navigate(path);
+  };
   return (
     <div id='portfolio'>
-        <h1>Portfolio</h1>
-        <div className='a-container'>
-            <Projectsbox image={fimage1} title="Characters"/>
-            <Projectsbox image={fimage2} title="Environments"/>
-            <Projectsbox image={fimage3} title="Visual effects"/>
-            <Projectsbox image={fimage4} title="3D models"/>
-        </div>
+      <h1>PORTFOLIOS</h1>
+        <div className='wrapper'>
+        <button className='features-button' onClick={() => handleButtonClick('/characters')}>
+          <Projectsbox image={fimage1} title="Characters"/>
+        </button>
+        <button className='features-button' onClick={() => handleButtonClick('/environments')}>
+          <Projectsbox image={fimage2} title="Environments"/>
+        </button>
+        <button className='features-button' onClick={() => handleButtonClick('/visual-effects')}>
+          <Projectsbox image={fimage3} title="Visual effects"/>
+        </button>
+        <button className='features-button' onClick={() => handleButtonClick('/3d-models')}>
+          <Projectsbox image={fimage4} title="3D models"/>
+        </button>
+        <button className='features-button' onClick={() => handleButtonClick('/characters')}>
+          <Projectsbox image={fimage1} title="Characters"/>
+        </button>
+        <button className='features-button' onClick={() => handleButtonClick('/environments')}>
+          <Projectsbox image={fimage2} title="Environments"/>
+        </button>
+        <button className='features-button' onClick={() => handleButtonClick('/visual-effects')}>
+          <Projectsbox image={fimage3} title="Visual effects"/>
+        </button>
+        <button className='features-button' onClick={() => handleButtonClick('/3d-models')}>
+          <Projectsbox image={fimage4} title="3D models"/>
+        </button>
+      </div>
     </div>
   )
 }
