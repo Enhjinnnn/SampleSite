4d05686a256ba2279b94a2a4059e72d7859cdbe5
enhjinnn <enhjinn08@gmail.com>
2023-07-28 19:00:10 +0800

    add character

--- src/ProjectCharacter.js
+++ src/ProjectCharacter.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, {Suspense} from 'react';
+import { useRef, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, useGLTF, useAnimations } from '@react-three/drei';
+
+
+export function Model(props) {
+  const group = useRef()
+  const { nodes, materials, animations } = useGLTF('/GirlAnimation.gltf')
+  const { actions } = useAnimations(animations, group)
+  return (
+    <group ref={group} {...props} dispose={null}>
+      <group name="Scene">
+        <group name="Armature" rotation={[Math.PI / 2, 0, 0]} scale={0.01}>
+          <primitive object={nodes.mixamorig1Hips} />
+          <skinnedMesh name="Ch29" geometry={nodes.Ch29.geometry} material={materials.Ch29_Body} skeleton={nodes.Ch29.skeleton} />
+        </group>
+      </group>
+    </group>
+  )
+}
 
 
 function ProjectCharacter() {
+  const ref=useRef()
   return (
-  <div className='name'>
-    <h1>This is ProjectCharacter page</h1>
-  </div>
+    <div className='Environment'>
+     <Canvas>
+      <Suspense fallback={null}>
+       <ambientLight/>
+       <spotLight intensity={0.9} angle={0.1} penumbra={1}
+                 position={[10,15,10]}
+                 castShadow/>
+       <Model/>
+       <OrbitControls enablePan={true}
+                      enableZoom={true}
+                      enableRotate={true}/>
+      </Suspense>
+     </Canvas>
+    </div>
   )
 }
 
