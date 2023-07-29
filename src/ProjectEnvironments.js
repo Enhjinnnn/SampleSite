19b7cd038cc9d98ff9ac95b11e3ff33c0ef6d2dd
enhjinnn <enhjinn08@gmail.com>
2023-07-29 17:51:26 +0800

    add scroll to top on environment page

--- src/ProjectEnvironments.js
+++ src/ProjectEnvironments.js
@@ -118,6 +118,10 @@ export function Model(props) {
 
 function ProjectEnvironments() {
   const ref=useRef()
+  useEffect(() => {
+    // Scroll to the top of the page when the component is mounted
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div className='Environment'>
      <Canvas>
